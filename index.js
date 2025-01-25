7ab49906c94533b6e07db8fbd0db55076ad691de
Tanvir <freelancertanvir91@gmail.com>
2025-01-26 03:25:05 +0600

    Add periodic HTTPS request and update dependencies

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const express = require('express')
 const router = require('./router')
 const app = express()
 const cors = require('cors')
-
+const https = require('https')
 
 
 app.use(cors())
 app.use(express.json({limit:"50mb"}))
 app.use('/', router)
 
+
+setInterval(() => {
+    https.get("https://mukhboddho-mern.onrender.com"); 
+    }, 2* 60 * 1000);
+
 mongoose.connect(process.env.DB).then(res=>console.log("connected to db")).catch(err=>console.log(err));
 
 
 
-app.listen(2084)
+app.listen()
