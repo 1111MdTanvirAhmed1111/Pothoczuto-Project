dff694fdc363c6bb35f18befc60ce93ff27d3537
Tanvir <freelancertanvir91@gmail.com>
2025-01-26 03:20:33 +0600

    Upload Files

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const express = require('express')
 const router = require('./router')
 const app = express()
 const cors = require('cors')
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+
 
 
 app.use(cors())
 app.use(express.json({limit:"50mb"}))
 app.use('/', router)
+
+mongoose.connect(process.env.DB).then(res=>console.log("connected to db")).catch(err=>console.log(err));
 
 
 
