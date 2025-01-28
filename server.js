52b42959797b224dba87c94a984bab42f5f22963
Tanvir <freelancertanvir91@gmail.com>
2025-01-29 01:50:27 +0600

    Update2.0

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const blogRoutes = require('./routes/blogRoutes');
-
+const cors = require('cors')
 const app = express();
 
 app.use(express.json());
 
+app.use(cors())
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', blogRoutes);
 
