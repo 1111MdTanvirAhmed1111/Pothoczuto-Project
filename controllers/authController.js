9e16bc4093f7f0a7ecef1e1ef6c3285fed620690
Tanvir <freelancertanvir91@gmail.com>
2025-02-22 00:52:10 +0600

    Add email uniqueness check and password hashing to user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,13 @@ exports.register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    const user = new User({ username, email, password });
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists.' });
+    }
+const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ username, email, password: hashedPassword });
     await user.save();
 
     res.status(201).json({ message: 'User registered successfully.' });
