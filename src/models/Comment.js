b2591628a882a5cb8b4adf9fad3051492902c00d
Tanvir <freelancertanvir91@gmail.com>
2025-02-22 23:27:12 +0600

    I apolog

---
+++ src/models/Comment.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const CommentSchema = new mongoose.Schema({
+  content: String,
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+  author: String,
+  date: { type: Date, default: Date.now },
+
+});
+
+export default mongoose.models.Comment || mongoose.model('Comment', CommentSchema);
+
