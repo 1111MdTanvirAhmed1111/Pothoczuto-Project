3c5133c89a6d2a21dc233bc6aa7545a7e2f97972
Tanvir <freelancertanvir91@gmail.com>
2025-03-16 22:36:45 +0600

    Fix typo in URL for portfolio backend in looperRunner

--- src/utils/looperRunner.js
+++ src/utils/looperRunner.js
@@ -3,7 +3,7 @@ const https = require('https');
 const runLooper = () => {
   setInterval(() => {
     https.get("https://pothoczuto-backend.onrender.com/");
-    https.get("hhttps://gazi-tanvir-portfolio.onrender.com/");
+    https.get("https://gazi-tanvir-portfolio.onrender.com/");
   }, 45 * 1000);
 };
 
